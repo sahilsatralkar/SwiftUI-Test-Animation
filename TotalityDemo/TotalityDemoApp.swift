8eb197f008eed1239d40a29544c700a6e14ca31e
Sahil Satralkar <sahil.satralkar@gmail.com>
2021-11-22 19:54:45 +0530

    Initial Commit

---
+++ TotalityDemo/TotalityDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TotalityDemoApp.swift
+//  TotalityDemo
+//
+//  Created by Sahil Satralkar on 22/11/21.
+//
+
+import SwiftUI
+
+@main
+struct TotalityDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
